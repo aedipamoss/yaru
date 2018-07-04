d54890413b75a40ca2caca88978b7e14d8a20989
Ædipa Moss <24195775+aedipamoss@users.noreply.github.com>
2018-07-04 13:40:14 +0900

    Pull out things routes into own namespace
    Use `wrap-json-response` middleware as the body for all responses.

--- src/yaru/core.clj
+++ src/yaru/core.clj
@@ -2,30 +2,28 @@
   (:require [cheshire.core :refer :all]
             [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.json :refer [wrap-json-body]]
-            [ring.util.response :refer [response]])
+            [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
+            [ring.util.response :refer [response]]
+            [yaru.thing :as things])
   (:use ring.adapter.jetty
         ring.middleware.params
-        ring.middleware.default-charset
-        yaru.thing))
+        ring.middleware.default-charset))
 
 (defn hello [name]
   (response (str "Hello " name)))
 
-(defn show-thing [id]
-  (response (str (get-thing id))))
-
 (defroutes handler
   (routes
    (GET "/hello/:name" [name] (hello name))
-   (GET "/thing/:id" [id] (show-thing id))
-   (POST "/thing" request (create-thing request))))
+   (GET "/thing/:id" [id] (things/get-thing id))
+   (POST "/thing" request (things/create-thing request))))
 
 (def app
   (-> handler
       wrap-params
       (wrap-default-charset "utf-8")
-      (wrap-json-body {:keywords? true})))
+      (wrap-json-body {:keywords? true})
+      wrap-json-response))
 
 (defn -main []
   (run-jetty app {:port 3000}))
